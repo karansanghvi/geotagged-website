ba6bba0d5595064bfdb586b990e5001619b0ff8c
Karan Sanghvi <karansanghvi1303@gmail.com>
2024-01-19 00:03:54 +0530

    css

--- FilterFunctions/productDisplay.js
+++ FilterFunctions/productDisplay.js
@@ -4,12 +4,18 @@ data.forEach((product, index) => {
     productCard.className = 'productCard';
     productCard.innerHTML = `
     <div class="product">
-        <img src="${product.img}" alt="${product.title}">
-        <h3>${product.title}</h3>
-        <p class="prevPrice">${product.prevPrice}</p>
-        <p>${product.newPrice}</p>
-        <p>Catgeory: ${product.category}</p>
-    </div>
+                    <img src="${product.img}" alt="${product.title}">
+                    <h3>${product.title}</h3>
+                    <div>
+                        <div class="product-grid">
+                            <p class="prevPrice">${product.prevPrice}</p>
+                        </div>
+                        <div>
+                            <p class="product-price">${product.newPrice}</p>
+                        </div>
+                    </div>
+                    <p>Catgeory: ${product.category}</p>
+                </div>
     `;
     productsContainer.appendChild(productCard);
     if ((index + 1) % 3 === 0) {

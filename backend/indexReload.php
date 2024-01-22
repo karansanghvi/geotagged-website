92613677c388a8240306b4d952b3d5fcdc624b6e
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-01-22 15:05:12 +0530

    Newsletter Backend and Login Button Toggle Complete

---
+++ backend/indexReload.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+$response = array();
+include "dbconnect.php";
+
+if(isset($_SESSION["userID"])){
+    $userID = $_SESSION['userID'];
+    $checkUserSQL = "SELECT * FROM `users` WHERE userID = '$userID'";
+    $checkUser = mysqli_query($conn, $checkUserSQL);
+    $userData = mysqli_fetch_row($checkUser);
+    $_SESSION['userID'] = $userData[0];
+    $_SESSION['firstName'] = $userData[1];
+    $_SESSION['lastName'] = $userData[2];
+    $_SESSION['userEmail'] = $userData[3];
+    $_SESSION['userMobile'] = $userData[4];
+    $_SESSION['userPassword'] = $userData[5];
+    $_SESSION['is_Subscribed'] = $userData[6];
+    $_SESSION['loggedIn'] = true;
+    
+    $isLoggedIn = $_SESSION['loggedIn'];  
+    $response['is_Subscribed'] = $userData[6];
+    $response['sessionData'] = $_SESSION;
+    $response['isLoggedIn'] = $isLoggedIn;
+}
+else{
+    $response['isLoggedIn'] = False;
+}
+
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
---
+++ backend/subscribeProcess.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$response = array();
+include "dbconnect.php";
+
+function checkUserSubscribed($conn, $userID) {
+    $checkUserSQL = "SELECT * FROM `users` WHERE userID = '$userID' and is_subscribed = 1";
+    $checkUser = mysqli_query($conn, $checkUserSQL);
+
+    if($checkUser && mysqli_num_rows($checkUser) == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+if (isset($_SESSION['loggedIn'])) {
+    $userID = $_SESSION['userID'];
+    if (!checkUserSubscribed($conn, $userID)) {
+        $subscribeUserSQL = "UPDATE `users` SET `is_subscribed` = '1' WHERE userID = '$userID';";
+        $result = mysqli_query($conn, $subscribeUserSQL);
+        
+        if ($result) {
+            $_SESSION["is_Subscribed"] = 1;            
+            $response['success'] = true;
+            $response['message'] = "User Subscribed";
+        } else {
+            $response['error'] = "Error: " . mysqli_error($conn);
+        }
+    } else {
+        $response['message'] = "User already Subscribed";
+    }
+    $response['sessionData'] = $_SESSION;
+} 
+else{
+    $response['message'] = "User not logged in.";
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
